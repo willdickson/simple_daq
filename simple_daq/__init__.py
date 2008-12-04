90f590dafeae945e31b9306538ae5d5387c7d98f
Will Dickson <wbd@caltech.edu>
2008-12-03 18:45:48 -0800

    Added license + some modification to INSTALL file.

--- simple_daq/__init__.py
+++ simple_daq/__init__.py
@@ -1,2 +1,25 @@
+"""
+simple_daq
+Copyright (C) William Dickson, 2008.
+
+wbd@caltech.edu
+www.willdickson.com
+
+This file is part of simple_daq.
+
+simple_step is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+    
+simple_step is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with simple_step.  If not, see
+<http://www.gnu.org/licenses/>.  
+"""
 import pkg_resources
 from simple_daq import *
--- simple_daq/simple_daq.py
+++ simple_daq/simple_daq.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python
 """
- simple_daq.py
+simple_daq
+Copyright (C) William Dickson, 2008.
 
- A simple module for capturing data from comedi supported data
- acquisition cards. Includes functions which support the daq_acquire
- command-line data acquisition program. In particular, in addition the
- basic data acquisition function "acquire_data" there are functions
- for parsing text based configuration files and handling command-line
- options.
+wbd@caltech.edu
+www.willdickson.com
+
+This file is part of simple_daq.
+
+simple_step is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+    
+simple_step is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with simple_step.  If not, see
+<http://www.gnu.org/licenses/>.  
+
+---------------------------------------------------------------------
+
+Purpose: A simple module for capturing data from comedi supported data
+acquisition cards. Includes functions which support the daq_acquire
+command-line data acquisition program. In particular, in addition the
+basic data acquisition function "acquire_data" there are functions for
+parsing text based configuration files and handling command-line
+options.
  
  William Dickson 02/20/2007
 
