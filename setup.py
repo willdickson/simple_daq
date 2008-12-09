1bbcc96cbe4e7c182205bdbf52384df9e1e9523b
Will Dickson <wbd@caltech.edu>
2008-12-08 18:09:07 -0800

    Fixed some problem in documentation.

--- setup.py
+++ setup.py
@@ -7,18 +7,18 @@ www.willdickson.com
 
 This file is part of simple_daq.
 
-simple_step is free software: you can redistribute it and/or modify it
+simple_daq is free software: you can redistribute it and/or modify it
 under the terms of the GNU Lesser General Public License as published
 by the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
     
-simple_step is distributed in the hope that it will be useful, but
+simple_daq is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 Lesser General Public License for more details.
 
 You should have received a copy of the GNU Lesser General Public
-License along with simple_step.  If not, see
+License along with simple_daq.  If not, see
 <http://www.gnu.org/licenses/>.  
 from setuptools import setup, find_packages
 
